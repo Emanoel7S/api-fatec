4e56245410e92d49baf3ae59b29a1511b311728a
Emanoel7S <emanoel.santos@fatec.sp.gov.br>
2024-03-06 19:20:49 -0300

    teste

--- src/main/java/br/com/api_fatec/api_fatec/ApiFatecApplication.java
+++ src/main/java/br/com/api_fatec/api_fatec/ApiFatecApplication.java
@@ -6,27 +6,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
 @RestController
 @SpringBootApplication
 public class ApiFatecApplication {
-	@RequestMapping("/")
-	String home() {
-		return "hello world";
-	}
-	@RequestMapping("/numero1")
-	int home3() {
-		return 10;
-	}
-	@RequestMapping("/numero/{num}")
-	Integer numero2(@PathVariable Integer num) {
-		return num;
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(ApiFatecApplication.class, args);
-		             
-					  
-	}
 
+    @RequestMapping("/")
+    String home() {
+        return "hello world";
+    }
+
+    @RequestMapping("/numero1")
+    int home3() {
+        return 10;
+    }
+
+    @RequestMapping("/numero/{num}")
+    Integer numero2(@PathVariable Integer num) {
+        return num;
+    }
+
+    @RequestMapping("/classificar/{idade}")
+    String classificarIdade(@PathVariable int idade) {
+        if (idade < 0) {
+            return "Idade inválida";
+        } else if (idade < 12) {
+            return "Criança";
+        } else if (idade >= 12 && idade <= 18) {
+            return "Adolescente";
+        } else if (idade >= 19 && idade <= 60) {
+            return "Adulto";
+        } else {
+            return "Idoso";
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFatecApplication.class, args);
+    }
 }
