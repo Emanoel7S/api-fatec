402cc0f6735741f293f96d7c6d084901ccc2600c
Emanoel7S <emanoel.santos@fatec.sp.gov.br>
2024-04-04 22:21:47 -0300

    feat: clintesv2

--- src/main/java/br/com/api_fatec/api_fatec/ApiFatecApplication.java
+++ src/main/java/br/com/api_fatec/api_fatec/ApiFatecApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api_fatec.api_fatec.domain.cliente.ClienteRepository;
